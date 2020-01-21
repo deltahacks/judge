f00c4a1f205e24e64d74eb2dd291b16aebf602f7
Anant Jain <anant.j2409@gmail.com>
2020-01-20 19:47:47 -0500

    Removed Rubric

--- src/components/LoginHeader.vue
+++ src/components/LoginHeader.vue
@@ -43,7 +43,6 @@
     <div class="nav" :class="{ open: showMenu }">
       <ul>
         <a href="/home"><li>Home</li></a>
-        <a href="/home"><li>Rubric</li></a>
         <a href="/home"><li>Rankings</li></a>
         <a href="#" @click="logout()" v-if="showLogout==1"><li>Logout</li></a>
       </ul>
